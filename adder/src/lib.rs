73e2597fe94acabb48d2d77c6e988346571bbf83
td1k <35556931+td1k@users.noreply.github.com>
2024-02-28 06:52:54 +0000

    Added example rust code

---
+++ adder/src/lib.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn basic_math() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn basic_math2() {
+        assert_eq!(3 + 2, 5);
+    }    
+}
